c4fb5df85806f89a4a20760dccf0ad8fd5d5e5ca
Academich <mihandronov@gmail.com>
2020-10-08 20:52:48 +0000

    Added first take on model class, loss function and training loop

--- model/model.py
+++ model/model.py
@@ -1,38 +1,75 @@
 import torch
-from torch import nn
+from torch import nn, Tensor
+from torch.utils.data import TensorDataset
+from torch.optim.optimizer import Optimizer
+
+from utils import EPS, get_q_joint
 
 
+def loss_function(p_joint: Tensor, q_joint: Tensor) -> Tensor:
+    """
+    Calculates KLDiv between joint distributions in original and embedding space
+    :param p_joint:
+    :param q_joint:
+    :return: KLDiv value
+    """
+    # TODO Add here alpha gradient calculation too
+    return (p_joint * torch.log((p_joint + EPS) / (q_joint + EPS))).sum()
 
 
+def fit_model(model: nn.Module,
+              x_and_p_joint: TensorDataset,
+              opt: Optimizer,
+              n_epochs: int,
+              batch_size: int) -> None:
+    """
+    Fit model, but the whole p_joint must be calculated
+    beforehand and then fed in by batches
+    :param model:
+    :param x_and_p_joint:
+    :param opt:
+    :param n_epochs:
+    :param batch_size:
+    :return:
+    """
+    model.train()
+    n_points = len(x_and_p_joint)
+    for epoch in range(n_epochs):
+        train_loss = 0
+        for i in range(n_points // batch_size + 1):
+            start_idx = i * batch_size
+            fin_idx = start_idx + min(batch_size, n_points - start_idx)
+            orig_points_batch, p_joint_batch = x_and_p_joint[start_idx: fin_idx]
+            opt.zero_grad()
+            embeddings = model(orig_points_batch)
+            q_joint_batch = get_q_joint(embeddings, alpha=1, dist_func="euc")
+            loss = loss_function(p_joint_batch, q_joint_batch)
+            train_loss += loss.item()
+            loss.backward()
+            opt.step()
+        print(f'====> Epoch: {epoch} Average loss: {train_loss / n_points:.4f}')
 
 
+def weights_init(m: nn.Module) -> None:
+    if isinstance(m, (nn.Linear,)):
+        nn.init.kaiming_normal(m.weight.data)
+        m.bias.data.fill_(0.01)
+    elif isinstance(m, (nn.BatchNorm1d,)):
+        m.weight.data.fill_(1.0)
+        m.bias.data.fill_(0)
 
 
+class NeuralMapping(nn.Module):
 
+    def __init__(self, dim_input, dim_emb=2):
+        super().__init__()
+        self.linear_1 = nn.Linear(dim_input, dim_input)
+        self.bn_1 = nn.BatchNorm1d(dim_input)
+        self.linear_2 = nn.Linear(dim_input, dim_emb)
+        self.relu = nn.ReLU()
 
+        self.apply(weights_init)
 
-# class LinearClassificator(nn.Module):
-#
-#     def __init__(self, dim_in, dim_out):
-#         super().__init__()
-#         self.lin = nn.Linear(dim_in, dim_out)
-#
-#     def forward(self, x):
-#         return self.lin(x)
-#
-#
-# def fit(model, train_ds, loss_func, optimizer, n_epochs, batch_size):
-#     n_points = len(train_ds)
-#     for epoch in range(n_epochs):
-#         losses = []
-#         for i in range(n_points // batch_size):
-#             from_i = i * batch_size
-#             to_i = from_i + min(batch_size, n_points - from_i)
-#             x_batch, y_batch = train_ds[from_i:to_i]
-#             preds = model(x_batch)
-#             loss = loss_func(preds, y_batch)
-#             losses.append(loss.item())
-#             loss.backward()
-#             optimizer.step()
-#             optimizer.zero_grad()
-#         print(f"Epoch {epoch + 1}. Mean Loss: {torch.tensor(losses).mean()}")
+    def forward(self, x):
+        x = self.relu(self.bn_1(self.linear_1(x)))
+        return self.linear_2(x)
