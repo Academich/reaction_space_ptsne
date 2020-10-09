cc595fab95214a02a6cb6d922c60a0b5d3c5fd38
Academich <mihandronov@gmail.com>
2020-10-09 20:28:01 +0300

    Added training on MNIST

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from torch import optim, tensor, matmul, sum, ones, cat, zeros
 from torch.utils.data import TensorDataset
 
 from datasets import load_mnist_some_classes
-from origin_distrib import OrigDistrBuilder
 from utils import make_joint
+from model.model import NeuralMapping, fit_model
+from plot_embeddings import plot_embs
 
 import datetime
 
 if __name__ == '__main__':
-    include_classes = (1, 2, 8)
+    include_classes = (1, 3, 4, 5, 6)
     points, labels = load_mnist_some_classes(include_classes)
     start = datetime.datetime.now()
-    p_cond_builder = OrigDistrBuilder(points, perplexity=30)
-    p_cond = p_cond_builder.calculate_optimized_p_cond(dist_func="euc")
-    p_joint = make_joint(p_cond)
+    ffnn = NeuralMapping(dim_input=points.size(1)).cuda()
+    opt = optim.SGD(ffnn.parameters(), lr=0.05, momentum=0.9)
+    fit_model(ffnn, points.cuda(), opt, perplexity=100, n_epochs=10, batch_size=200)
     fin = datetime.datetime.now()
     print("time elapsed:", fin - start)
+    ffnn.eval()
+    final_embs = ffnn(points.cuda()).cpu().detach().numpy()
+    plot_embs(final_embs, labels.numpy())
+
+
