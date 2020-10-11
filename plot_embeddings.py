ba3dad561dfbb5ff4f1040dbf7a12ad94c9defe0
Academich <mihandronov@gmail.com>
2020-10-11 14:22:32 +0300

    Now plotting embeddings space before and after training

--- plot_embeddings.py
+++ plot_embeddings.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-def plot_embs(final_embs, labels=None):
+def plot_embs(initial_embs, final_embs, labels=None):
+    x_init = initial_embs[:, 0]
+    y_init = initial_embs[:, 1]
     x = final_embs[:, 0]
     y = final_embs[:, 1]
-    plt.figure()
-    plt.scatter(x, y, c=labels, s=2)
-    plt.title("Пространство представлений t-SNE")
+    fig, ax = plt.subplots(nrows=1, ncols=2)
+    ax[0].scatter(x_init, y_init, c=labels, s=2)
+    ax[0].set_title("Before training")
+    ax[1].scatter(x, y, c=labels, s=2)
+    ax[1].set_title("After training")
+    plt.suptitle("Пространство представлений t-SNE")
     plt.show()
