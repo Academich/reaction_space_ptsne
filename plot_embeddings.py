073d93be5292968608e4317bfbff0c1501d0ee06
Academich <mihandronov@gmail.com>
2020-10-12 16:41:52 +0300

    Changed cmap

--- plot_embeddings.py
+++ plot_embeddings.py
@@ -7,9 +7,9 @@ def plot_embs(initial_embs, final_embs, labels=None):
     x = final_embs[:, 0]
     y = final_embs[:, 1]
     fig, ax = plt.subplots(nrows=1, ncols=2)
-    ax[0].scatter(x_init, y_init, c=labels, s=2)
+    ax[0].scatter(x_init, y_init, c=labels, s=2, cmap="hsv")
     ax[0].set_title("Before training")
-    ax[1].scatter(x, y, c=labels, s=2)
+    ax[1].scatter(x, y, c=labels, s=2, cmap="hsv")
     ax[1].set_title("After training")
     plt.suptitle("Final embedding space")
     plt.show()
