48005297e53b8d065fd4f57dae9c67924fd7cc7c
Academich <mihandronov@gmail.com>
2020-10-15 14:04:47 +0300

    Model is now evaluated in plot_embs and points are rendered there too

--- plot_embeddings.py
+++ plot_embeddings.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
+from torch.nn import Module
+from torch.utils.data import Dataset
+from model.model import get_batch_embeddings
+from config import config
 
 
-def plot_embs(initial_embs, final_embs, labels=None):
-    x_init = initial_embs[:, 0]
-    y_init = initial_embs[:, 1]
-    x = final_embs[:, 0]
-    y = final_embs[:, 1]
+def plot_embs(trained_model: Module,
+              untrained_ref_model: Module,
+              ds: Dataset,
+              bs=config.training_params["batch_size"]):
     fig, ax = plt.subplots(nrows=1, ncols=2)
-    ax[0].scatter(x_init, y_init, c=labels, s=2, cmap="hsv")
+    for ref_embs_batch, labels_batch in get_batch_embeddings(untrained_ref_model, ds, bs):
+        x_init = ref_embs_batch[:, 0]
+        y_init = ref_embs_batch[:, 1]
+        ax[0].scatter(x_init, y_init, c=labels_batch, s=2, cmap="hsv")
+    for trained_embs_batch, labels_batch in get_batch_embeddings(trained_model, ds, bs):
+        x = trained_embs_batch[:, 0]
+        y = trained_embs_batch[:, 1]
+        ax[1].scatter(x, y, c=labels_batch, s=2, cmap="hsv")
     ax[0].set_title("Before training")
-    ax[1].scatter(x, y, c=labels, s=2, cmap="hsv")
     ax[1].set_title("After training")
     plt.suptitle("Final embedding space")
     plt.show()
