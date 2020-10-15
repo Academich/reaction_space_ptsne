cc13f2ac78d1c91ce62a4f9f1f39c5abef124582
Academich <mihandronov@gmail.com>
2020-10-15 14:40:41 +0300

    Запущено на смайлсах

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import torch
-from torch.utils.data import TensorDataset
-from datasets import load_mnist_some_classes
-from model.model import NeuralMapping, fit_model, get_batch_embeddings
+from datasets import SmilesDataset
+from model.model import NeuralMapping, fit_model
 from plot_embeddings import plot_embs
 from config import config
 
 import datetime
 
 if __name__ == '__main__':
-    # Defining data
-    include_classes = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-    points, labels = load_mnist_some_classes(include_classes)
-    dim_input = points.size(1)
+
+    n_bits = 1024
+    points_ds = SmilesDataset("data/nuclear.smi", n_bits=n_bits)
+    dim_input = n_bits
 
     # Defining instruments
     if config.seed:
@@ -20,8 +19,6 @@ if __name__ == '__main__':
     ffnn = NeuralMapping(dim_input=dim_input).to(dev)
     untrained_ref_ffnn = NeuralMapping(dim_input=dim_input).to(dev)
     opt = torch.optim.Adam(ffnn.parameters(), **config.optimization_conf)
-    points = points.to(dev)
-    points_ds = TensorDataset(points)
 
     # Training and evaluating
     start = datetime.datetime.now()
