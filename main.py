f9a9a5f5207cf4d56c53b66e7758bfdf6ff40de0
Academich <mihandronov@gmail.com>
2020-10-15 13:26:49 +0300

    Reorganized with Dataset class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data import TensorDataset, DataLoader
 from datasets import load_mnist_some_classes
 from model.model import NeuralMapping, fit_model
 from plot_embeddings import plot_embs
@@ -7,24 +8,31 @@ from config import config
 import datetime
 
 if __name__ == '__main__':
-    # Defining dataset
+    # Defining data
     include_classes = (1, 2, 3, 4, 5, 6, 7, 8, 9)
     points, labels = load_mnist_some_classes(include_classes)
+    dim_input = points.size(1)
 
     # Defining instruments
     if config.seed:
         torch.manual_seed(config.seed)
     dev = torch.device(config.dev)
-    ffnn = NeuralMapping(dim_input=points.size(1)).to(dev)
+    ffnn = NeuralMapping(dim_input=dim_input).to(dev)
     opt = torch.optim.Adam(ffnn.parameters(), **config.optimization_conf)
+    points = points.to(dev)
+    points_ds = TensorDataset(points)
 
-    start = datetime.datetime.now()
     # Training and evaluating
-    init_embs = ffnn(points.to(dev)).cpu().detach().numpy()
-    fit_model(ffnn, points.to(dev), opt, **config.training_params)
-    fin = datetime.datetime.now()
-    ffnn.eval()
-    final_embs = ffnn(points.to(dev)).cpu().detach().numpy()
+    start = datetime.datetime.now()
 
+    init_embs = ffnn(points).cpu().detach().numpy()
+    ffnn.train()
+    fit_model(ffnn, points_ds, opt, **config.training_params)
+    ffnn.eval()
+    final_embs = ffnn(points).cpu().detach().numpy()
+
+    fin = datetime.datetime.now()
     print("time elapsed:", fin - start)
+
+    # Plotting result
     plot_embs(init_embs, final_embs, labels)
