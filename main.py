f75b63d76665e12273f2bd5b1a8a55d496ac5b53
Academich <mihandronov@gmail.com>
2020-10-21 18:20:43 +0300

    Сделал код универсальным для химич. датасетов и для мниста, а также добавил более грубокую модель

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
-from datasets import SmilesDataset
-from model.model import NeuralMapping, fit_model
+from datasets import SmilesDataset, load_mnist_some_classes
+from torch.utils.data import TensorDataset
+from model.model import NeuralMapping, fit_model, NeuralMappingDeeper
 from plot_embeddings import plot_embs
 from config import config
 
@@ -14,23 +15,28 @@ if __name__ == '__main__':
     dev = torch.device(config.dev)
     print(dev)
 
-    # TODO В конфиг это
-    n_bits = 4096
-    filepath = "data/nuclear.smi"
-    save_flag = False
+    if config.data['name'] != "mnist":
+        n_bits = config.data['n_bits']
+        dataset_name = f"data/{config.data['name']}.smi"
 
-    print(filepath)
-    points_ds = SmilesDataset(filepath, dev, n_bits=n_bits)
-    dim_input = n_bits
+        points_ds = SmilesDataset(dataset_name, dev, n_bits=n_bits)
+        dim_input = n_bits
+    else:
+        include_classes = None
+        points, labels = load_mnist_some_classes(include_classes)
+        dim_input = points.size(1)
+        points = points.to(dev)
+        points_ds = TensorDataset(points, labels)
 
+    print(config.data['name'])
     ffnn = NeuralMapping(dim_input=dim_input).to(dev)
-    untrained_ref_ffnn = NeuralMapping(dim_input=dim_input).to(dev)
+    untrained_ref_ffnn = NeuralMappingDeeper(dim_input=dim_input).to(dev)
     opt = torch.optim.Adam(ffnn.parameters(), **config.optimization_conf)
 
     # Training and evaluating
     start = datetime.datetime.now()
 
-    fit_model(ffnn, points_ds, opt, **config.training_params, save_model_flag=save_flag)
+    fit_model(ffnn, points_ds, opt, **config.training_params, save_model_flag=config.save_flag)
 
     fin = datetime.datetime.now()
     print("Training time:", fin - start)
