466eba3233c3986820481e24a70cfb4e8d4bc8e5
Academich <mihandronov@gmail.com>
2020-10-21 18:19:52 +0300

    Добавил несколько секций

--- config.py
+++ config.py
@@ -9,8 +9,10 @@ class Config:
     def __init__(self):
         self.dev = conf_dict["device"]
         self.seed = conf_dict["seed"]
+        self.save_flag = bool(conf_dict['save_model'])
         self.optimization_conf = conf_dict["optimization"]
         self.training_params = conf_dict["training"]
+        self.data = conf_dict["dataset"]
 
 
 config = Config()
