1ac29417ea45ce69e61c9bb4edd1a1f7db586d70
Academich <mihandronov@gmail.com>
2020-11-01 16:56:57 +0300

    Небольшое веб-приложение для рендеринга молекул и реакций в браузере через бокех

---
+++ visual_evaluation/server.py
@@ -0,0 +1,36 @@
+from base64 import b64decode
+from rdkit import Chem
+from flask import Flask, send_file
+from rdkit.Chem import rdDepictor, Draw
+from rdkit.Chem import AllChem
+from io import BytesIO
+
+app = Flask(__name__)
+
+
+@app.route("/render_molecule/<image>.svg")
+def render_molecule(image):
+    mol = Chem.MolFromSmiles(b64decode(image).decode('ascii'))
+    rdDepictor.Compute2DCoords(mol)
+    mc = Chem.Mol(mol.ToBinary())
+    Chem.Kekulize(mc)
+    drawer = Draw.MolDraw2DSVG(100, 100)
+    drawer.DrawMolecule(mc)
+    drawer.FinishDrawing()
+    svg = drawer.GetDrawingText().replace(':svg', '').replace('svg:', '')
+    return send_file(BytesIO(svg.encode('ascii')), mimetype='image/svg+xml')
+
+
+@app.route("/render_reaction/<image>.svg")
+def render_reaction(image):
+    smarts = b64decode(image).decode('ascii')
+    rxn = AllChem.ReactionFromSmarts(smarts, useSmiles=True)
+    drawer = Draw.MolDraw2DSVG(900, 300)
+    drawer.DrawReaction(rxn)
+    drawer.FinishDrawing()
+    svg = drawer.GetDrawingText().replace(':svg', '').replace('svg:', '')
+    return send_file(BytesIO(svg.encode('ascii')), mimetype='image/svg+xml')
+
+
+if __name__ == '__main__':
+    app.run()
