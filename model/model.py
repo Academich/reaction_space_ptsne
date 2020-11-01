ee1099d7ede899c408de8acaee75b16ee2fa2b4a
Academich <mihandronov@gmail.com>
2020-11-01 17:54:44 +0300

    Модели теперь сохраняются под случайным именем и вместе с json-файлом, в котором записаны настройки обучения, чтобы не забыть

--- model/model.py
+++ model/model.py
@@ -1,11 +1,12 @@
 import datetime
+import json
 
 import torch
 from torch import nn, Tensor
 from torch.optim.optimizer import Optimizer
 from torch.utils.data import DataLoader, Dataset
 
-from utils.utils import EPS, get_q_joint, calculate_optimized_p_cond, make_joint
+from utils.utils import EPS, get_q_joint, calculate_optimized_p_cond, make_joint, get_random_string
 
 
 def loss_function(p_joint: Tensor, q_joint: Tensor) -> Tensor:
@@ -33,7 +34,8 @@ def fit_model(model: nn.Module,
               bin_search_max_iter: int,
               min_allowed_sig_sq: float,
               max_allowed_sig_sq: float,
-              save_model_flag: bool = True
+              save_model_flag: bool,
+              configuration_report: str
               ) -> None:
     """
     Fits t-SNE model
@@ -53,11 +55,13 @@ def fit_model(model: nn.Module,
     :param min_allowed_sig_sq: Minimal allowed value for squared sigmas
     :param max_allowed_sig_sq: Maximal allowed value for squared sigmas
     :param save_model_flag: A flag whather to save model or not
+    :param configuration_report: Config of the model in string form for report purposes
     :return:
     """
     model.train()
     n_points = len(input_points)
     train_dl = DataLoader(input_points, batch_size=batch_size, shuffle=True)
+    model_name = get_random_string(6)
     for epoch in range(n_epochs):
         epoch_start_time = datetime.datetime.now()
         train_loss = 0
@@ -86,10 +90,12 @@ def fit_model(model: nn.Module,
             opt.step()
         epoch_end_time = datetime.datetime.now()
         time_elapsed = epoch_end_time - epoch_start_time
-        if save_model_flag and (epoch + 1) % 5 == 0:
-            _path = "model/" + f"model_dist_{dist_func_name}_per_{perplexity}_bs_{batch_size}_epoch_{epoch + 1}.pt"
-            save_model(model, path=_path)
-            print("Model is saved")
+        if save_model_flag and (epoch + 1) % 10 == 0:
+            save_path = "model/" + f"{model_name}_epoch_{epoch + 1}"
+            torch.save(model, save_path + ".pt")
+            with open(save_path + ".json", "w") as here:
+                json.dump(json.loads(configuration_report), here)
+            print('Model saved as %s' % save_path)
         print(f'====> Epoch: {epoch + 1}. Time {time_elapsed}. Average loss: {train_loss / n_points:.4f}')
 
 
@@ -110,17 +116,6 @@ def get_batch_embeddings(model: nn.Module,
         with torch.no_grad():
             embeddings = model(batch_points)
             yield embeddings, batch_labels
-
-
-def save_model(model: nn.Module, path: str) -> None:
-    """
-    Saves model in a .pt file
-    :param model:
-    :param path:
-    :return:
-    """
-    torch.save(model, path)
-    print('Model saved as %s' % path)
 
 
 def weights_init(m: nn.Module) -> None:
