5dbf6c28ca11e903cd1a209d6282c410fc403257
mandronov <mihandronov@gmail.com>
2021-03-14 13:28:04 +0300

    created new dataset that build BERT descriptors for reactions

--- datasets.py
+++ datasets.py
@@ -2,7 +2,16 @@ from typing import Dict, Any
 
 import torch
 from torch.utils.data import Dataset
+from rxnfp.transformer_fingerprints import (
+    RXNBERTFingerprintGenerator,
+    get_default_model_and_tokenizer
+)
+
 from utils.reactions import reaction_fps
+
+model, tokenizer = get_default_model_and_tokenizer()
+
+rxnfp_generator = RXNBERTFingerprintGenerator(model, tokenizer)
 
 
 class ReactionSmilesDataset(Dataset):
@@ -37,3 +46,35 @@ class ReactionSmilesDataset(Dataset):
                                    **self.params)
 
         return torch.from_numpy(descriptors).float().to(self.dev), self.labels[idx]
+
+
+class BERTFpsReactionSmilesDataset(Dataset):
+
+    def __init__(self,
+                 filepath: str,
+                 no_agents: bool,
+                 dev: Any) -> None:
+        self.filepath = filepath  # path to .csv file
+        self.dev = dev
+        self.smiles = []
+        self.labels = []
+        with open(self.filepath) as _file:
+            for i, line in enumerate(_file):
+                try:
+                    smi, label = line.split(";")
+                except ValueError:
+                    smi = line
+                    label = 0
+                smi = smi.strip()
+                if no_agents:
+                    reactants, agents, products = smi.split(">")
+                    smi = f"{reactants}.{agents}>>{products}"
+                self.smiles.append(smi)
+                self.labels.append(int(label))
+
+    def __len__(self):
+        return len(self.smiles)
+
+    def __getitem__(self, idx):
+        bert_fingerprint = rxnfp_generator.convert(self.smiles[idx])
+        return torch.tensor(bert_fingerprint).float().to(self.dev), self.labels[idx]
