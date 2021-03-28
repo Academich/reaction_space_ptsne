dd2032c6eec928afee69dd64c8b0a4e04c8331ed
mandronov <mihandronov@gmail.com>
2021-03-29 00:03:17 +0300

    added general tanimoto distance

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,7 @@
 import random
 import string
 from math import log2
-from torch import tensor, eye, ones, device, isnan
+from torch import tensor, eye, ones, device, isnan, zeros
 from torch import max as torch_max
 from config import config
 
@@ -25,11 +25,12 @@ def squared_euc_dists(x: tensor) -> tensor:
     return sq_norms + sq_norms.unsqueeze(1) - 2 * x @ x.t()
 
 
-def squared_jaccard_distances(x: tensor) -> tensor:
+def jaccard_distances(x: tensor) -> tensor:
     """
-    Calculates squared jaccard dissimilarities between rows
+    Calculates jaccard dissimilarities between rows.
+    Rows should be binary vectors. Pretty fast function.
     :param x: Matrix of input points (n_points, n_dimensions)
-    :return: Matrix of squared jaccard dissimilarities between x_i and x_j (n_points, n_points)
+    :return: Matrix of jaccard dissimilarities between x_i and x_j (n_points, n_points)
     """
     n_ones = x.sum(dim=1)
     intersection = x @ x.t()
@@ -38,17 +39,60 @@ def squared_jaccard_distances(x: tensor) -> tensor:
     return 1 - similarity
 
 
+def squared_jaccard_distances(x: tensor) -> tensor:
+    """
+    Calculates squared jaccard dissimilarities between rows.
+    Rows should be binary vectors. Pretty fast function.
+    :param x: Matrix of input points (n_points, n_dimensions)
+    :return: Matrix of squared jaccard dissimilarities between x_i and x_j (n_points, n_points)
+    """
+    return jaccard_distances(x) ** 2
+
+
+def general_jaccard_distances(x: tensor) -> tensor:
+    """
+    Calculates jaccard dissimilarities between rows.
+    Rows may be continuous vectors. Pretty slow function
+    :param x: Matrix of input points (n_points, n_dimensions)
+    :return: Matrix of jaccard dissimilarities between x_i and x_j (n_points, n_points)
+    """
+    lesser = (x < x.unsqueeze(1))
+    greater = lesser.__invert__()
+    lesser = lesser.float()
+    greater = greater.float()
+    minimums = (x * lesser + x.unsqueeze(1) * greater).sum(2)
+    maximums = (x * greater + x.unsqueeze(1) * lesser).sum(2)
+    similarity = minimums / maximums
+    return 1 - similarity
+
+
+def squared_general_jaccard_distances(x: tensor) -> tensor:
+    """
+    Calculates squared jaccard dissimilarities between rows.
+    Rows may be continuous vectors. Pretty slow function
+    :param x: Matrix of input points (n_points, n_dimensions)
+    :return: Matrix of squared jaccard dissimilarities between x_i and x_j (n_points, n_points)
+    """
+    return general_jaccard_distances(x) ** 2
+
+
 def squared_cosine_distances(x: tensor) -> tensor:
     raise NotImplementedError
 
 
 distance_functions = {"euc": squared_euc_dists,
                       "jaccard": squared_jaccard_distances,
+                      "jaccard_general": squared_general_jaccard_distances,
                       "cosine": squared_cosine_distances}
 
 
+def initialize_multiscale_p_joint(batch_size: int) -> tensor:
+    res = zeros(batch_size, batch_size).to(device(config.dev))
+    return res
+
+
 def calculate_optimized_p_cond(input_points: tensor,
-                               perplexity: int,
+                               target_entropy: float,
                                dist_func: str,
                                tol: float,
                                max_iter: int,
@@ -58,8 +102,8 @@ def calculate_optimized_p_cond(input_points: tensor,
     Adjust sigmas for every row in conditional probability matrix
     to match the given perplexity using binary search
     :param input_points: Unoptimized conditional probability matrix
-    :param perplexity: The hyperparameter that governs the dispersion of
-    conditional probabilities.
+    :param target_entropy: The entropy that every distribution (row) in conditional
+    probability matrix will be optimized to match
     :param dist_func: A string denoting the desired distance function
     :param tol: The tolerance threshold for binary search
     :param max_iter: Number of maximum iterations for binary search
@@ -69,7 +113,7 @@ def calculate_optimized_p_cond(input_points: tensor,
     """
 
     n_points = input_points.size(0)
-    target_entropy = log2(perplexity)
+
     diag_mask = (1 - eye(n_points)).to(device(config.dev))
 
     dist_f = distance_functions[dist_func]
@@ -86,7 +130,7 @@ def calculate_optimized_p_cond(input_points: tensor,
     curr_iter = 0
     while not finished.all().item():
         if curr_iter >= max_iter:
-            print("Warning! Exceeded max iter.", flush=True)
+            print(f"Warning! Exceeded max iter. Not optimized: {(~finished).sum().item()}", flush=True)
             # print("Discarding batch")
             return p_cond
         pos_diff = (ent_diff > 0).float()
