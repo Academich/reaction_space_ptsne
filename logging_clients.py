ff8d2fbf141efd4f1787fd22bafa6fe95e5037e7
Academich <mihandronov@gmail.com>
2022-05-05 17:03:56 +0200

    Added an option to specify run name.

--- logging_clients.py
+++ logging_clients.py
@@ -1,9 +1,14 @@
+from typing import Optional
+
 import wandb
 
 from pytorch_lightning.loggers import WandbLogger
 
 
-def make_wandb_logger(project_name: str):
+def make_wandb_logger(project_name: str, run_name: Optional[str] = None):
     wandb.init(project=project_name)
+    if run_name is not None:
+        wandb.run.name = run_name
+        wandb.run.save()
     wandb_logger = WandbLogger()
     return wandb_logger
