e27dbc05c18ff356213393ae57c5eaaf423a65e1
Academich <mihandronov@gmail.com>
2022-05-05 17:04:45 +0200

    Added callbacks for saving the model, fixed a small mistake with loggers, added help messages for the CLI args. #6

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import torch
 import pytorch_lightning as pl
 
 from src import RxnDataModule, PTSNEMapper
-import logging_clients
 
 # === Initialize arguments parser ===
 parser = ArgumentParser()
 
 # PROGRAM level args
-parser.add_argument("--seed", type=int, default=123456)
-parser.add_argument("--save_dir_path", type=str, default="saved_models")
-parser.add_argument("--use_wandb", action="store_true", default=False)
-parser.add_argument("--wandb_project_name", type=str, default="reaction-space-ptsne")
+parser.add_argument("--seed", type=int, default=123456,
+                    help="Random seed for reproducibility.")
+parser.add_argument("--save_dir_path", type=str, default="saved_models",
+                    help="Path to the folder with the saved models.")
+parser.add_argument("--use_wandb", action="store_true", default=False,
+                    help="A flag whether to use W&B for logging or not.")
+parser.add_argument("--wandb_project_name", type=str, default="reaction-space-ptsne",
+                    help="The project name for W&B.")
+parser.add_argument("--wandb_run_name", type=str, default=None,
+                    help="The name of the run. Used by W&B and as a model checkpoint name.")
 
 # add model specific args
 parser = PTSNEMapper.add_model_specific_args(parser)
@@ -30,19 +35,29 @@ pl.seed_everything(args.seed)
 torch.backends.cudnn.determinstic = True
 torch.backends.cudnn.benchmark = False
 
+# === Callbacks ===
+checkpoint_callback = pl.callbacks.ModelCheckpoint(dirpath=args.save_dir_path,
+                                                   verbose=True,
+                                                   save_last=True)
+progress_bar_callback = pl.callbacks.progress.TQDMProgressBar(refresh_rate=args.progress_bar_refresh_rate)
+cb_list = [checkpoint_callback, progress_bar_callback]
+
 # === Instantiate classes
 data_module = RxnDataModule.from_argparse_args(args)
 
 dim_input = args.n_bits if args.fp_method != "transformer" else 256
 model = PTSNEMapper.from_argparse_args(args, dim_input=dim_input)
 
-logger = None
+# === Logger clients (W&B or Tensorboard)
+logger, run_name = None, args.wandb_run_name
 if args.use_wandb:
-    logger = logging_clients.make_wandb_logger(args.wandb_project_name)
+    import logging_clients
+    logger = logging_clients.make_wandb_logger(args.wandb_project_name, run_name)
+
 trainer = pl.Trainer.from_argparse_args(args,
                                         logger=logger,
                                         profiler=None,
-                                        callbacks=None)
+                                        callbacks=cb_list)
 
 if __name__ == '__main__':
     trainer.fit(model, data_module)
